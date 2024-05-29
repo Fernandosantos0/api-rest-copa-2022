ad36e98932023fa0368fc6f181f1ca9b5fded015
Fernando Santos <fernandomeca7@gmail.com>
2024-05-29 15:41:39 -0300

    Persistência de Dados com o MySQL, e criando o arquivo de conexão como banco de dados

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 import app from './src/app.js';
 import colors from 'colors';
 
-// escutar a porta 3000
-const PORT = 3000;
-const HOST = 'localhost';
+// importando a conexão com o banco de dados
+import conexao from './infra/conexao.js';
 
-app.listen(PORT, HOST, () => {
-    console.log(`Servidor rodando no endereço http://${HOST}:${PORT}`.green.bold);
+
+
+// fazer a conexão
+conexao.getConnection((erro) => {
+    if(erro) {
+        console.log('Não foi possível conectar com o banco de dados'.red.bold);
+        console.error(erro);
+    } else {
+        console.log('Conexão como banco de dados realizado com sucesso'.blue.bold);
+
+        /* Subindo o servidor */
+        const PORT = 3000;
+        const HOST = 'localhost';
+        app.listen(PORT, HOST, () => {
+            console.log(`Servidor rodando no endereço http://${HOST}:${PORT}`.green.bold);
+        });
+        
+    }
 });
