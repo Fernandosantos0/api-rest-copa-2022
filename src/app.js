a402b830a807aab2651a75370bce82eabfa63d41
Fernando Santos <fernandomeca7@gmail.com>
2024-05-29 14:50:54 -0300

    Criar Rotas GET com parâmetro no Express, Criar rota DELETE no Express

--- src/app.js
+++ src/app.js
@@ -6,10 +6,20 @@ const app = express();
 // mock
 const selecoes = [
     {id: 1, selecao: 'Brasil', grupo: 'G'},
-    {id: 1, selecao: 'Suíça', grupo: 'G'},
-    {id: 1, selecao: 'Sérvia', grupo: 'G'},
-    {id: 1, selecao: 'Camarões', grupo: 'G'},
+    {id: 2, selecao: 'Suíça', grupo: 'G'},
+    {id: 3, selecao: 'Camarões', grupo: 'G'},
+    {id: 4, selecao: 'Sérvia', grupo: 'G'}
 ];
+
+// retornar o objeto por id
+function buscarSelecaoPorId(id) {
+    return selecoes.filter(selecao => selecao.id == id);
+}
+
+// pegar a posição do index do elemento no array por id
+function buscarIndexSelecao(id) {
+    return selecoes.findIndex(selecao => selecao.id == id);
+}
 
 // indicar para o express ler body
 app.use(bodyParser.urlencoded({
@@ -26,9 +36,25 @@ app.get('/selecoes', (req, res, next) => {
     res.status(200).json(selecoes);
 });
 
+app.get('/selecoes/:id', (req, res, next) => {
+    const { id } = req.params;
+
+    res.status(200).json(buscarSelecaoPorId(id));
+});
+
 app.post('/selecoes', (req, res, next) => {
     selecoes.push(req.body);
     res.status(201).send('Seleção cadastrada com sucesso!');
+});
+
+app.delete('/selecoes/:id', (req, res, next) => {
+    const { id } = req.params;
+    let index = buscarIndexSelecao(id);
+
+    // remover o elemento
+    selecoes.splice(index, 1);
+
+    res.status(200).send(`Seleção com id ${id} excluída comsucesso`);
 });
 
 export default app;
